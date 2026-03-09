f8d9bd44f2d2bf907c802e4a19fca4fd2599477f
Higor Chagas <higornchagas@gmail.com>
2026-03-09 15:28:59 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,106 @@
+import express from "express";
+import db from "./database.js";
+import { randomBytes } from "crypto";
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/order", (req, res) => {
+  const { value } = req.body;
+  const orderId = randomBytes(9).toString("base64url");
+
+  db.run(
+    "INSERT INTO Orders (orderId, value) VALUES (?, ?)",
+    [orderId, value],
+    function (err) {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      res.json({
+        orderId,
+        value,
+      });
+    },
+  );
+});
+
+app.get("/order/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.get(`SELECT * FROM Orders WHERE orderId = ?`, [id], (err, row) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    if (!row) {
+      return res.status(404).json({ error: "Pedido não encontrado" });
+    }
+
+    res.json(row);
+  });
+});
+
+app.get("/orders", (req, res) => {
+  db.all("SELECT * FROM Orders", [], (err, rows) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    res.json(rows);
+  });
+});
+
+app.put("/order/:id", (req, res) => {
+  const id = req.params.id;
+  const { value } = req.body;
+
+  db.run(
+    "UPDATE Orders SET value = ? WHERE orderId = ?",
+    [value, id],
+    function (err) {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      if (this.changes === 0) {
+        return res.status(404).json({ error: "Pedido não encontrado" });
+      }
+
+      res.json({
+        message: "Pedido atualizado",
+        orderId: id,
+        value,
+      });
+    },
+  );
+});
+
+app.delete("/order/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.run("DELETE FROM Orders WHERE orderId = ?", [id], function (err) {
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    if (this.changes === 0) {
+      return res.status(404).json({ error: "Pedido não encontrado" });
+    }
+
+    res.json({
+      message: "Pedido deletado com sucesso",
+      orderId: id,
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ database.js
@@ -0,0 +1,25 @@
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database("./database.db");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS Orders (
+      orderId TEXT PRIMARY KEY,
+      value REAL,
+      creationDate TEXT DEFAULT CURRENT_TIMESTAMP
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS Items (
+      orderId INTEGER,
+      productId INTEGER,
+      quantity INTEGER,
+      price REAL,
+      FOREIGN KEY(orderId) REFERENCES Orders(orderId)
+    )
+  `);
+});
+
+export default db;
